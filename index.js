d1eb924e6c6be6376ec9dadd6de7ee50e4da0dee
Shubham.c <shubham.c@finacus.co.in>
2024-05-07 18:17:54 +0530

    first update mongo_express

---
+++ index.js
@@ -0,0 +1,32 @@
+const express =  require('express');
+const app = express();
+const path =  require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/products');
+mongoose.connect('mongodb://localhost:60724/testdb')
+.then(()=>{console.log('connection established :>> ', );})
+.catch(()=>{console.log('error in conneection :>> ', );})
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs');
+app.get('/test',(req,res)=>{
+    res.send('Lets Go!');
+})
+// --------------
+app.get('/products',async (req,res)=>{
+   const products = await Product.find({})
+    console.log(products,'products');
+    res.render('products/index',{products});
+})
+
+app.get('/products/:id',async(req,res)=>{
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    console.log(product,'product ID');
+    // res.send('details page')
+    res.render('products/show',{product})
+})
+app.listen(7777,()=>{
+    console.log('App is listening on port - 7777 :>> ', );
+})
+
---
+++ seeds.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Product = require('./models/products');
+mongoose.connect('mongodb://localhost:60724/testdb')
+    .then(() => { console.log('connection established :>> ',); })
+    .catch(() => { console.log('error in conneection :>> ',); })
+
+
+const seedProducts = [
+    {
+        name: 'Orange',
+        price: 40,
+        category: 'fruit'  
+    },
+    {
+        name: 'Apple',
+        price: 60,
+        category: 'fruit'
+    },
+    {
+        name: 'Watermelon',
+        price: 70,
+        category: 'fruit'
+    },
+    {
+        name: 'Carrot',
+        price: 50,
+        category: 'vegetables'
+    },
+    {
+        name: 'milk',
+        price: 10,
+        category: 'dairy'
+    }
+]
+
+Product.insertMany(seedProducts).then(p => {
+    console.log('p :>> ', p);
+}).catch((e) => { console.log(e, 'error'); });
+
+
+
+// const p = new Product({
+//     name:'Grapes',
+//     price:50,
+//     category:'fruit'
+// })
+// p.save().then(p =>{
+//     console.log('p :>> ', p);
+// }).catch((e)=>{console.log(e,'error');})

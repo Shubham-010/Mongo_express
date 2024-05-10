dfdc653d9a4a699142a8ef1df227a99dd7c96227
Shubham.c <shubham.c@finacus.co.in>
2024-05-10 16:43:38 +0530

    Final CRUD funcctionlity added

--- index.js
+++ index.js
@@ -13,37 +13,42 @@ mongoose.connect('mongodb://localhost:27017/dbtest')
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs');
 app.use(methodOverride('_method'))
+
 //when there is post request we dont have access to body directly
 app.use(express.urlencoded({extended:true})); //to connect middleware express to the body in html
 
-app.get('/test',(req,res)=>{
-    res.send('Lets Go!');
-})
+
 const categories = ['fruit','vegetables','dairy']
-// --------------
+// ------Display all products--------
 app.get('/products',async (req,res)=>{
-   const products = await Product.find({})
-    console.log(products,'products');
-    res.render('products/index',{products});
+    const {category} = req.query;
+    if(category){
+        const products = await Product.find({category})
+        res.render('products/index',{products,category});
+    }else{
+        const products = await Product.find({})
+        console.log(products,'products');
+        res.render('products/index',{products,category:"All"});
+    }
 })
-// ------------
+// -------Access the New product form-----
 app.get('/products/new',async(req,res)=>{
     res.render('products/new', { categories })
 })
-// -----------
+// -------find and edit ----
 app.get('/products/:id/edit',async(req,res)=>{
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product, categories })
 })
-// ------------
+// -------Update a product--------
 app.put('/products/:id',async(req,res)=>{
     const {id} = req.params;
     const product = await Product.findByIdAndUpdate(id,req.body,{runValidators:true}) //edit the product
     console.log(product);
     res.redirect(`/products/${product._id}`)
 })
-// ------------
+// ----------Add data in database--------------------
 app.post('/products',async(req,res)=>{
     console.log(req.body,"REQ");
     const newProduct =  new Product(req.body);
@@ -51,15 +56,21 @@ app.post('/products',async(req,res)=>{
     res.redirect(`/products/${newProduct._id}`)
 
     console.log(newProduct,"newProduct");
-    // res.send('making your products')
 })
-// -----------------
+// -------Access Product Details----------
 app.get('/products/:id',async(req,res)=>{
     const {id} = req.params;
     const product = await Product.findById(id);
     console.log(product,'product ID');
     // res.send('details page')
     res.render('products/show',{product})
+})
+// --------Delete Product--------
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deleteproduct = await Product.findByIdAndDelete(id);
+    // res.send('details page')
+    res.redirect('/products')
 })
 // ----------------
 app.listen(7777,()=>{
